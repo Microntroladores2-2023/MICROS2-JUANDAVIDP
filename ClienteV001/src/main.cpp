4b346563889b664cf308a0a30162015388adfc5f
juand2207 <juandavid150596@gmail.com>
2023-06-24 18:54:00 -0400

    Updated

--- ClienteV001/src/main.cpp
+++ ClienteV001/src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+// Network information
+const char* ssid = "JuanD";
+const char* password = "ingucv2207";
+const char* serverIP = "172.16.0.4"; // IP de PC
+const int serverPort = 1234;  // puerto del programa que se use como servidor
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+   Serial.begin(115200);
+   WiFi.begin(ssid, password);
+
+   while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando al Wifi");
+  }
+
+  Serial.println("Conectando al Wifi");
+
+  WiFiClient client;
+
+  Serial.print("Conectando al Servidor");
+  Serial.println(serverIP);
+
+  if (!client.connect(serverIP, serverPort)){
+    Serial.println("Conexion fallida");
+    return;
+  }
+
+  Serial.println("Conectado al Servidor");
+  client.println("Hola servidor! Bienvenido");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
